a712f13e386e5b311c66db284359f45f119c4d1b
Ajitkumar-25 <kajit0408@gmail.com>
2024-07-22 16:47:26 +0530

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+import "./App.css";
+import Resume from "./components/Resume";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export function App() {
+  const [user, setUser] = useState({});
+  const [verified, setVerified] = useState(true);
+
+  useEffect(() => {
+    async function tokenVerifier() {
+      if (user?.email) {
+        const data = {
+          token: user.token,
+          email: user.email,
+        };
+        await axios
+          .post("/verifyToken", data)
+          .then((res) => {
+            if (res.status === 200) setVerified(true);
+          })
+          .catch((err) => {
+            const errorStatus = err.response.status;
+            if (errorStatus === 401) setVerified(false);
+          });
+      }
+    }
+
+    tokenVerifier(user);
+  }, [user]);
+
+  useEffect(() => {
+    if (!verified) {
+      localStorage.removeItem("user");
+      window.location.replace("/");
+    }
+  }, [verified]);
+
+  useEffect(() => {
+    const theUser = localStorage.getItem("user");
+    if (theUser && !theUser.includes("undefined")) {
+      setUser(JSON.parse(theUser));
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor:"#e3f2fd",minHeight:"100vh"}}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={user?.email ? <Resume user={user} /> : <Home />}
+          ></Route>
+          <Route
+            exact
+            path="/signup"
+            element={user?.email ? <Resume user={user} /> : <Signup />}
+          ></Route>
+          <Route
+            exact
+            path="/login"
+            element={user?.email ? <Resume user={user} /> : <Login />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
